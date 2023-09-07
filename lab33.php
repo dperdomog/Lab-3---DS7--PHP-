2d2e42ec93ceb12799348d8ba74e3cf59ed9ca05
dperdomog <daekdj@gmail.com>
2023-09-06 21:07:14 -0500

    Lab 3 - DS7

---
+++ lab33.php
@@ -0,0 +1,41 @@
+<html>
+    <head>
+    <title>Laboratorio 3.3</title>
+    </head>
+    <body>
+        <?PHP
+        
+        if (array_key_exists('enviar', $_POST)) {
+        if ($_REQUEST['Apellido'] != "" )
+        {
+            echo "El apellido Ingresado es: $_REQUEST [Apellido]";
+        }
+        else
+        {
+            echo "Favor coloque el apellido";
+        }
+
+        echo "<BR>";
+
+
+        if ($_REQUEST['Nombre'] != "" )
+        {
+            echo "El nombre Ingresado es: $_REQUEST[Nombre]";
+        }
+        else
+        {
+            echo "Favor coloque el nombre";
+        }
+        }
+        else{
+        ?>
+        <FORM ACTION = "lab33.php" METHOD="POST">
+        Nombre: <INPUT TYPE="TEXT" NAME="Nombre"><br>
+        Apellido:<INPUT TYPE = "TEXT" NAME="Apellido"><br>
+        <INPUT TYPE="SUBMIT" NAME="enviar" VALUE="Enviar datos"> 
+        </FORM>
+    <?PHP
+    }
+    ?>
+    </body> 
+</html>
